971cb4d1d1f42ed830cb84540b83ce73e01421df
Tao Zhang <134069397+cquzhangtao@users.noreply.github.com>
2024-02-28 04:08:21 +0100

    Delete trunk/run.sh

--- trunk/run.sh
+++
@@ -1,4 +0,0 @@
-$ cat .bashrc
-export LD_LIBRARY_PATH="/home/i11etazh/i11etazh/program/scons/lib:/home/i11etazh/i11etazh/program/serf/lib:/home/i11etazh/i11etazh/program/lz4/lib:/home/i11etazh/i11etazh/program/apruti/lib:/home/i11etazh/i11etazh/program/apr/lib:/home/i11etazh/i11etazh/program/termcap/lib:/home/i11etazh/i11etazh/program/readline/lib:/home/i11etazh/i11etazh/program/libffi/lib64:/home/i11etazh/i11etazh/program/libffi/lib:/home/i11etazh/i11etazh/program/ssl/lib64":$LD_LIBRARY_PATH
-export PATH=/home/i11etazh/i11etazh/program/svn/bin:/home/i11etazh/i11etazh/program/python/bin:/home/i11etazh/i11etazh/program/scons/bin:$PATH
-python3 -m com.tao.py.rl_example.TrainAllAgents /home/i11etazh 1
